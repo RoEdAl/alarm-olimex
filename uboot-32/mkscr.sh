42471a86e68179ff11abfeffb90c1511e7c2b878
Edmunt Pienkowsky <roed@onet.eu>
2020-08-31 12:40:35 +0200

    uboot-olimex-32: update mkscr script
    Preserve timestamps.

--- uboot-32/mkscr.sh
+++ uboot-32/mkscr.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 if [[ ! -x /usr/bin/mkimage ]]; then
   echo "mkimage not found. Please install uboot-tools:"
@@ -6,4 +6,6 @@ if [[ ! -x /usr/bin/mkimage ]]; then
   exit 1
 fi
 
-mkimage -A arm -O linux -T script -C none -n "U-Boot boot script" -d boot.txt boot.scr
+SOURCE_TS=$(date -r boot.txt -u '+%s')
+env SOURCE_DATE_EPOCH=${SOURCE_TS} mkimage -A arm -O linux -T script -C none -n "Arch Linux ARM" -d boot.txt boot.scr
+touch -c -d @${SOURCE_TS} boot.scr
